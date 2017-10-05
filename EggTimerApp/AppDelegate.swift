b8c5b02fa85c3633190906b2e0f3a5f4276e7478
Dimitar Danailov <dimityr.danailov@gmail.com>
2017-10-05 13:14:11 +0300

    Initial Commit

---
+++ EggTimerApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EggTimerApp
+//
+//  Created by Dimitar Danailov on 5.10.17.
+//  Copyright © 2017 Dimitar Danailov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EggTimerApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimerApp
+//
+//  Created by Dimitar Danailov on 5.10.17.
+//  Copyright © 2017 Dimitar Danailov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
