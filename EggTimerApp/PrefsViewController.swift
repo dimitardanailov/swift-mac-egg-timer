f1d627d9d2403d14c8e7013307a6c81fb00ed43e
Dimitar Danailov <dimityr.danailov@gmail.com>
2017-10-09 10:02:03 +0300

    I completed part 2

--- EggTimerApp/PrefsViewController.swift
+++ EggTimerApp/PrefsViewController.swift
@@ -10,9 +10,24 @@ import Cocoa
 
 class PrefsViewController: NSViewController {
 
+    @IBOutlet weak var presetsPopup: NSPopUpButton!
+    @IBOutlet weak var customSlider: NSSlider!
+    @IBOutlet weak var customTextField: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+    @IBAction func popupValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func okButtonClicked(_ sender: Any) {
+    }
 }
