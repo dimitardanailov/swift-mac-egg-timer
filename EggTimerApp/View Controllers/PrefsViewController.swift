27ab856f5ac9bebb0261b319ada6fe3e99e2df38
Dimitar Danailov <dimityr.danailov@gmail.com>
2017-10-10 14:17:50 +0300

    PrefsViewController has a bridge and communicate with Prefs Model.

--- EggTimerApp/View Controllers/PrefsViewController.swift
+++ EggTimerApp/View Controllers/PrefsViewController.swift
@@ -19,17 +19,72 @@ class PrefsViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        showExistingPrefs()
     }
     
-    @IBAction func popupValueChanged(_ sender: Any) {
+    // 1
+    @IBAction func popupValueChanged(_ sender: NSPopUpButton) {
+        if sender.selectedItem?.title == "Custom" {
+            customSlider.isEnabled = true
+            return
+        }
+        
+        let newTimerDuration = sender.selectedTag()
+        customSlider.integerValue = newTimerDuration
+        showSliderValueAsText()
+        customSlider.isEnabled = false
     }
     
+    // 2
     @IBAction func sliderValueChanged(_ sender: Any) {
+        showSliderValueAsText()
     }
     
+    // 3
     @IBAction func cancelButtonClicked(_ sender: Any) {
+        view.window?.close()
     }
     
     @IBAction func okButtonClicked(_ sender: Any) {
+        saveNewPrefs()
+        view.window?.close()
+    }
+    
+    func saveNewPrefs() {
+        prefs.selectedTime = customSlider.doubleValue * 60
+        NotificationCenter.default.post(
+            name: Notification.Name(rawValue: "PrefsChanged"),
+            object: nil
+        )
+    }
+    
+    func showExistingPrefs() {
+        // 1
+        let selectedTimeInMinutes = Int(prefs.selectedTime) / 60
+        
+        // 2
+        presetsPopup.selectItem(withTitle: "Custom")
+        customSlider.isEnabled = true
+        
+        // 3
+        for item in presetsPopup.itemArray {
+            if item.tag == selectedTimeInMinutes {
+                presetsPopup.select(item)
+                customSlider.isEnabled = false
+                break
+            }
+        }
+        
+        // 4
+        customSlider.integerValue = selectedTimeInMinutes
+        showSliderValueAsText()
+    }
+    
+    // 5
+    func showSliderValueAsText() {
+        let newTimerDuration = customSlider.integerValue
+        let minutesDescription = (newTimerDuration == 1) ? "minute": "minutes"
+        customTextField.stringValue = "\(newTimerDuration) \(minutesDescription)"
     }
 }
